8df56babcd9eeb2c66afb87e53016c6c8d4c500d
Camilo-Forero <ka_hilo13@hotmail.com>
2022-03-14 16:15:16 -0500

    added fonts and transforming percentages on containers and borders

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>basicCSS</title>
+    <link rel="stylesheet" type="text/css" href="css/caviarDreams.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="css/basic.css">
+</head>
+<body>
+<div class="rowContainer1 corporativeColorWhite fontCaviar">
+    <!-- <div class="rowContainer1 displayFlex alignContentTopCenter contentCenter">
+        <div class="standardSquare">
+
+        </div>
+    </div> -->
+    <div class="rowContainer1 displayFlex alignContentTopCenter contentCenter">
+        <div  class="containerWidth10vw containerHeight10vw borderRadius5px borderStyle borderWidthHalf">
+            <div class="height15p width100p borderRadiusTopLeft5px corporativeColorGreen borderRadiusTopRight5px">
+                <div class="rowContainer1 displayFlex alignContentTopCenter contentLeft borderBox paddingHalf fontColorWhite fontSize1Half">
+                    hello
+                </div>
+            </div>
+            <div class="height45p width100p">
+                <div class="height100p width100p">
+                    <div class="rowContainer1 displayGrid basicColumn1">
+                        <div class="width100p height100p borderBox paddingHalf">
+                                <div class="fontSize1Half width100p">tocayo</div>
+                                <input type="text" class="width100p height1vw">
+                        </div>
+                        <div class="width100p height100p borderBox paddingHalf">
+                            <div class="width100p">
+                                <div class="fontSize1Half width100p">tocayo</div>
+                                <input type="text" class="width100p height1vw">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="height40p width100p borderRadiusBottomLeft5px borderRadiusBottomRight5px">
+                <div class="rowContainer1 displayFlex alignContentTopCenter contentCenter">
+                    <div class="height30p width80p borderRadius5px corporativeColorGreen">
+                        <div class="rowContainer1 displayFlex alignContentTopCenter contentCenter fontColorWhite fontSize1Half">
+                            entrar
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
